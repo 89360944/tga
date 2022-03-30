f509427283442fb17f6bb2c8c39a311c2de8fe57
89360944@qq.com <89360944@qq.com>
2022-03-30 13:34:54 +0800

    Initial commit

---
+++ src/main/java/com/example/tga/Web.java
@@ -0,0 +1,13 @@
+package com.example.tga;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Web {
+
+    @RequestMapping("/")
+    public String test() {
+        return "hello world";
+    }
+}
